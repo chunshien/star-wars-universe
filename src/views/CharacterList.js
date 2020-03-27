7a76e29feedf0bf452d0f190b4e4eebdefefe6b9
Chun Shien <chunshien.ng@embedcard.com>
2020-03-27 14:53:23 +0800

    adding react router

---
+++ src/views/CharacterList.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+function CharacterList() {
+  return (
+    <Fragment>
+      <div>
+        Character List
+      </div>
+    </Fragment>
+  );
+}
+
+export default CharacterList;
