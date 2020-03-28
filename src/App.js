d3e1ca4a4b273e114dbfe6ee03f53a6a819c7df3
Chun Shien <chunshien.ng@embedcard.com>
2020-03-29 00:49:09 +0800

    redux saga with simple list ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import CharacterList from './views/CharacterList.js';
+import CharacterList from './views/Character/CharacterList';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga'
+import reducer from './reducers'
+import rootSaga from './sagas'
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+// const action = type => store.dispatch({type});
 
 function App() {
   return (
-    <Router>
-      <div className="App">            
-        <Route exact path="/" component={CharacterList} />
-        <Route path="/character-list" component={CharacterList} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route exact path="/" component={CharacterList} />
+          <Route path="/character-list" component={CharacterList} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
